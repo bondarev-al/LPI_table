2f80965c65e7ba1fd0619e4ef7b9e306eb1626a8
Bondarev Alexander <bondarev-al@mail.ru>
2020-11-24 18:29:22 +0300

    Переделал оформление кода

--- test.c
+++ test.c
@@ -6,33 +6,53 @@
 
 #define PORT_BASE 0x2030
 #define PORT_STATUS PORT_BASE + 1
+#define END_X_BIT 64 
+#define START_X_BIT 32
+#define END_Y_BIT 8
+#define START_Y_BIT 16
 
-int x = 0, y = 0;
+// int x = 0, y = 0;
 
-int check_position()
+struct ZScanTable
+{
+  int x;
+  int y;
+  int start_x;
+  int end_x;
+  int start_y;
+  int end_y;
+  int max_x;
+  int max_y;
+};
+
+int zscan_table_check_position(struct ZScanTable *table)
 {
   int answ = inb(PORT_STATUS);
-  // if (answ != 0x10) return -1;
+  table->start_x = answ & START_X_BIT;
+  table->end_x   = answ & END_X_BIT;
+  table->start_y = answ & START_Y_BIT;
+  table->end_y   = answ & END_Y_BIT;
   return answ;
 }
 
-void put_data()
+void zscan_table_put_data(struct ZScanTable *table)
 {
-  outb((((0xcc >> (x & 3)) & 15) << 4) | ((0xcc >> (y & 3)) & 15), PORT_BASE);
+  outb((((0xcc >> (table->x & 3)) & 15) << 4) | ((0xcc >> (table->y & 3)) & 15), PORT_BASE);
 }
 
-int move_x(int d_x, int delay)
+int zscan_table_move_x(struct ZScanTable *table, int d_x, int delay)
 {
-  int answ = check_position();
+  zscan_table_check_position(table);
   if (d_x > 0)
   {
     //проверка не упремся ли при начале движения
-    if (answ & 64) return -1;
+    if (table->end_x) return -1;
     for(int i = 0; i < d_x; i++)
     { 
-      x++;
-      put_data();
-      if (check_position() & 64) return -1;
+      table->x++;
+      zscan_table_put_data(table);
+      zscan_table_check_position(table);
+      if (table->end_x) return -1;
       usleep(delay);
     }
     outb(0x00, PORT_BASE);
@@ -42,12 +62,13 @@ int move_x(int d_x, int delay)
   if (d_x < 0)
   {
     //проверка не упремся ли при начале движения
-    if (answ & 32) return -1;
+    if (table->start_x) return -1;
     for(int i = 0; i > d_x; i--)
     { 
-      x--;
-      put_data();
-      if (check_position() & 32) return -1;
+      table->x--;
+      zscan_table_put_data(table);
+      zscan_table_check_position(table);
+      if (table->start_x) return -1;
       usleep(delay);
     }
     outb(0x00, PORT_BASE);
@@ -59,18 +80,19 @@ int move_x(int d_x, int delay)
   
 }
 
-int move_y(int d_y, int delay)
+int zscan_table_move_y(struct ZScanTable *table, int d_y, int delay)
 {
-  int answ = check_position();
+  zscan_table_check_position(table);
   if (d_y > 0)
   {
     //проверка не упремся ли при начале движения
-    if (answ & 8) return -1;
+    if (table->end_y) return -1;
     for(int i = 0; i < d_y; i++)
     { 
-      y++;
-      put_data();
-      if (check_position() & 8) return -1;
+      table->y++;
+      zscan_table_put_data(table);
+      zscan_table_check_position(table);
+      if (table->end_y) return -1;
       usleep(delay);
     }
     outb(0x00, PORT_BASE);
@@ -79,35 +101,172 @@ int move_y(int d_y, int delay)
 
   if (d_y < 0)
   {
-    // этот выключатель не работает
     //проверка не упремся ли при начале движения
-    if (answ & 4) return -1;
+    if (table->start_y) return -1;
     for(int i = 0; i > d_y; i--)
     { 
-      y--;
-      put_data();
-      if (check_position() & 4) return -1;
+      table->y--;
+      zscan_table_put_data  (table);
+      zscan_table_check_position(table);
+      if (table->start_y) return -1;
       usleep(delay);
     }
     outb(0x00, PORT_BASE);
-    return 0; 
+    return 0;
   }
 
   outb(0x00, PORT_BASE);
   return 0;
 }
 
-int move_to_start(int delay)
+//не тестировал
+int zscan_table_calibrate(struct ZScanTable *table, int delay)
 {
-  move_x(-x, delay);
-  move_y(-y, delay);
+  int max_x = 0;
+  zscan_table_check_position(table);
+  while (!table->start_x)
+  {
+    table->x--;
+    zscan_table_put_data(table);
+    zscan_table_check_position(table);
+    usleep(delay);
+  }
+  outb(0x00, PORT_BASE);
+  zscan_table_check_position(table);
+  while (!table->end_x)
+  {
+    table->x++;
+    max_x++;
+    zscan_table_put_data(table);
+    zscan_table_check_position(table);
+    usleep(delay);
+  }
+  outb(0x00, PORT_BASE);
+  table->max_x = max_x;
+
+  int max_y = 0;
+  zscan_table_check_position(table);
+  while (!table->start_y)
+  {
+    table->y--;
+    zscan_table_put_data(table);
+    zscan_table_check_position(table);
+    usleep(delay);
+  }
+  outb(0x00, PORT_BASE);
+  zscan_table_check_position(table);
+  while (!table->end_y)
+  {
+    table->y++;
+    max_y++;
+    zscan_table_put_data(table);
+    zscan_table_check_position(table);
+    usleep(delay);
+  }
+  outb(0x00, PORT_BASE);
+  table->max_y = max_y;
+
+  zscan_table_move_x(table, - max_x / 2, 2000);
+  zscan_table_move_y(table, - max_y / 2, 2000);
+  table->x = 0;
+  table->y = 0;
+
   return 0;
 }
 
-int setup()
+// int finish_move(int start_x, int start_y, int d_x, int d_y, int delay)
+// {
+//   int dif_x, dif_y;
+//   dif_x = x - start_x;
+//   dif_y = y - start_y;
+//   if (dif_x != d_x) return move_x(d_x - dif_x, delay);
+//   else if (dif_y != d_y) return move_y(d_y - dif_y, delay);
+// }
+
+// int move_x_y(int d_x, int d_y, int delay)
+// {
+//   int temp_x = x, temp_y = y;
+//   int answ = check_position();
+//   if ((d_x >= 0) && (d_y >= 0))
+//   {
+//     if ((answ & END_X_BIT) || (answ & END_Y_BIT))  return -1;
+//     for(int i = 0, j = 0; (i < d_x) && (j < d_y); i++, j++)
+//     { 
+//       x++;
+//       y++;
+//       put_data();
+//       int temp_answ = check_position();
+//       if ((temp_answ & END_X_BIT) || (temp_answ & END_Y_BIT)) return -1;
+//       usleep(delay);
+//     }
+//     outb(0x00, PORT_BASE);
+//     return finish_move(temp_x, temp_y, d_x, d_y, delay);
+//   }
+//   if ((d_x >= 0) && (d_y < 0))
+//   {
+//     if ((answ & END_X_BIT) || (answ & START_Y_BIT))  return -1;
+//     for(int i = 0, j = 0; (i < d_x) && (j > d_y); i++, j--)
+//     { 
+//       x++;
+//       y--;
+//       put_data();
+//       int temp_answ = check_position();
+//       if ((temp_answ & END_X_BIT) || (temp_answ & START_Y_BIT)) return -1;
+//       usleep(delay);
+//     }
+//     outb(0x00, PORT_BASE);
+//     return finish_move(temp_x, temp_y, d_x, d_y, delay);
+//   }
+//   if ((d_x < 0) && (d_y >= 0))
+//   {
+//     if ((answ & START_X_BIT) || (answ & END_Y_BIT))  return -1;
+//     for(int i = 0, j = 0; (i > d_x) && (j < d_y); i--, j++)
+//     { 
+//       x--;
+//       y++;
+//       put_data();
+//       int temp_answ = check_position();
+//       if ((temp_answ & START_X_BIT) || (temp_answ & END_Y_BIT)) return -1;
+//       usleep(delay);
+//     }
+//     outb(0x00, PORT_BASE);
+//     return finish_move(temp_x, temp_y, d_x, d_y, delay);
+//   }
+//   if ((d_x < 0) && (d_y < 0))
+//   {
+//     if ((answ & START_X_BIT) || (answ & START_Y_BIT))  return -1;
+//     for(int i = 0, j = 0; (i > d_x) && (j > d_y); i--, j--)
+//     { 
+//       x--;
+//       y--;
+//       put_data();
+//       int temp_answ = check_position();
+//       if ((temp_answ & START_X_BIT) || (temp_answ & START_Y_BIT)) return -1;
+//       usleep(delay);
+//     }
+//     outb(0x00, PORT_BASE);
+//     return finish_move(temp_x, temp_y, d_x, d_y, delay);
+//   }
+// }
+
+int zscan_table_move_to_start(struct ZScanTable *table, int delay)
+{
+  zscan_table_move_x(table, -table->x, delay);
+  zscan_table_move_y(table, -table->y, delay);
+  return 0;
+}
+
+int zscan_table_setup(struct ZScanTable *table)
 {
   outb(0x00, PORT_BASE);
   outb(0x02, PORT_BASE + 2);
+  table->x = 0;
+  table->y = 0;
+}
+
+int zscan_table_exit(struct ZScanTable *table)
+{
+  outb(0x00, PORT_BASE);
 }
 
 int main(void)
@@ -122,25 +281,14 @@ int main(void)
     return errno;
   }
 
-  setup();
+  struct ZScanTable table;
 
-  move_x(-1000, 2000);
-  // move_y(1000, 2000);
-  // move_to_start(2000);
+  zscan_table_setup(&table);
 
-  // int answ = check_position();
-  // printf("answ = %x\n", answ);
-  // printf("st = %i, end = %i\n", (answ & 4), (answ & 8));
+  zscan_table_move_y(&table, 500, 2000);
+  zscan_table_move_to_start(&table, 2000);
 
-  // int answ = 0;
-  // while( 1 )
-  // {
-  //   usleep(1000000);
-  //   answ = inb(PORT_STATUS);
-  //   printf("answ = %x\n", answ);
-  // }
-
-  outb(0x00, PORT_BASE);
+  zscan_table_exit(&table);
   
   return 0;
 }
